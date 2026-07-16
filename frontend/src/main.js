ebe9f5a614e56066bb53228f80aa448abd849872
Danaé A.M. <dev.danae@gmail.com>
2026-07-16 15:28:12 +0900

    feat: update theme styles and add font imports for improved typography

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import i18n from "./services/i18n.js";
+import "@fontsource-variable/inter";
+import "@fontsource/ibm-plex-mono";
 
 createApp(App).use(i18n).mount("#app");
